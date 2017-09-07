03a17d04b42fa3e8ac1e90467830cf5b6604e323
Sebas <sebastian.buitrago@gmail.com>
2017-09-07 18:08:10 -0500

    Funciones finales

--- js/index.js
+++ js/index.js
@@ -16,13 +16,13 @@ var Calculadora = {
 		}
 	),
 	/*animar los botones al hacer click*/
-	animacionuno: function(tecla){
+	animacion1: function(tecla){
 		document.getElementById(tecla).style.transform="scale(0.9)";
 		setTimeout(function() {document.getElementById(tecla).style.transform="scale(1)";}, 200);
 	},
 	/*funcion restar*/
 		menos: function(){
-			this.animacionuno("menos");
+			this.animacion1("menos");
 			this.num1 = Number(this.pantalla);
 			this.pantalla = "",
 			this.opcion = 2,
@@ -35,7 +35,7 @@ var Calculadora = {
 	},
 	/*funcion multiplica*/
 		por: function(){
-			this.animacionuno("por");
+			this.animacion1("por");
 			this.num1 = Number(this.pantalla),
 			this.pantalla = "",
 			this.opcion = 3,
@@ -48,7 +48,7 @@ var Calculadora = {
 	},
 	/*funcion dividir*/
 		dividido: function(){
-			this.animacionuno("dividido");
+			this.animacion1("dividido");
 			this.num1 = Number(this.pantalla),
 			this.pantalla = "",
 			this.opcion = 4,
@@ -61,7 +61,7 @@ var Calculadora = {
 	},
 	/*funcion sumar*/
 	mas: function(){
-			this.animacionuno("mas");
+			this.animacion1("mas");
 			this.num1 += Number(this.pantalla),
 			this.pantalla = "",
 			this.opcion = 1,
@@ -71,6 +71,142 @@ var Calculadora = {
 			this.auxestado = 0,
 			this.decimal = 0,
 			this.viewdisplay();
- }
-}
-Calculadora.inicio();
+		},
+	  /*funcion igual*/
+	  igual: function(){
+	 	 this.animacion1("igual");
+	 	 switch(this.opcion){
+	 		 case 1:
+	 				 if(this.auxestado == 0){
+	 					 this.auxnum = Number(this.pantalla),
+	 					 this.pantalla = this.num1 + Number(this.pantalla),
+	 					 this.auxestado = 1,
+	 					 this.num1 = 0;
+	 				 }else{
+	 					 this.pantalla = Number(this.pantalla)+this.auxnum;
+	 				 }
+	 			 break;
+	 		 case 2:
+	 				 if(this.auxestado == 0){
+	 					 this.auxnum = Number(this.pantalla),
+	 					 this.pantalla = this.num1 - Number(this.pantalla),
+	 					 this.auxestado = 1,
+	 					 this.num1 = 0;
+	 				 }else{
+	 					 this.pantalla = Number(this.pantalla)-this.auxnum;
+	 				 }
+	 			 break;
+	 		 case 3:
+	 				 if(this.auxestado == 0){
+	 					 this.auxnum = Number(this.pantalla),
+	 					 this.pantalla = this.num1 * Number(this.pantalla),
+	 					 this.auxestado = 1,
+	 					 this.num1 = 0;
+	 				 }else{
+	 					 this.pantalla = Number(this.pantalla)*this.auxnum;
+	 				 }
+	 			 break;
+	 		 case 4:
+	 				 if(this.auxestado == 0){
+	 					 this.auxnum = Number(this.pantalla),
+	 					 this.pantalla = this.num1 / Number(this.pantalla),
+	 					 this.auxestado = 1,
+	 					 this.num1 = 0;
+	 				 }else{
+	 					 this.pantalla = Number(this.pantalla)/this.auxnum;
+	 				 }
+	 			 break;
+	 		 default:
+	 			 break;
+	 	 }
+	 	 this.viewdisplay();
+	  },
+	  /* Se asignan teclas*/
+	  EventosClick: function(){
+	 	 document.getElementById("0").addEventListener("click",function(){Calculadora.viewnum("0")});
+	 	 document.getElementById("1").addEventListener("click",function(){Calculadora.viewnum("1")});
+	 	 document.getElementById("2").addEventListener("click",function(){Calculadora.viewnum("2")});
+	 	 document.getElementById("3").addEventListener("click",function(){Calculadora.viewnum("3")});
+	 	 document.getElementById("4").addEventListener("click",function(){Calculadora.viewnum("4")});
+	 	 document.getElementById("5").addEventListener("click",function(){Calculadora.viewnum("5")});
+	 	 document.getElementById("6").addEventListener("click",function(){Calculadora.viewnum("6")});
+	 	 document.getElementById("7").addEventListener("click",function(){Calculadora.viewnum("7")});
+	 	 document.getElementById("8").addEventListener("click",function(){Calculadora.viewnum("8")});
+	 	 document.getElementById("9").addEventListener("click",function(){Calculadora.viewnum("9")});
+	 	 document.getElementById("on").addEventListener("click",function(){Calculadora.on("")});
+	 	 document.getElementById("sign").addEventListener("click",function(){Calculadora.sign()});
+	 	 document.getElementById("dividido").addEventListener("click",function(){Calculadora.dividido()});
+	 	 document.getElementById("menos").addEventListener("click",function(){Calculadora.menos()});
+	 	 document.getElementById("punto").addEventListener("click",function(){Calculadora.punto()});
+	 	 document.getElementById("igual").addEventListener("click",function(){Calculadora.igual()});
+	 	 document.getElementById("mas").addEventListener("click",function(){Calculadora.mas()});
+	 	 document.getElementById("por").addEventListener("click",function(){Calculadora.por()});
+	  },
+	  /* funcion vision de calculadora*/
+	  viewnum: function(valor){
+	 	 this.animacion1(valor);
+	 	 if(this.signo == 1 && this.stop == 0){
+	 		 this.controlen += 1,
+	 		 this.stop = 1;
+	 	 }
+	 	 if(this.decimal == 1  && this.stop == 0){
+	 		 this.controlen += 1,
+	 		 this.stop = 1;
+	 	 }
+	 	 if(this.pantalla.length < this.controlen){
+	 		 if(this.pantalla != "0"){
+	 			 this.pantalla += valor;
+	 		 }else if(valor != 0){
+	 			 this.pantalla = "",
+	 			 this.pantalla += valor;
+	 		 }
+	 		 this.viewdisplay();
+	 	 }
+	  },
+	  /* funcion limpieza*/
+	  on: function(){
+	 	 this.animacion1("on");
+	 	 this.pantalla = "0",
+	 	 this.decimal = 0,
+	 	 this.signo = 0,
+	 	 this.stop = 0,
+	 	 this.controlen = 8
+	 	 this.num1 = 0,
+	 	 this.auxestado = 0,
+	 	 this.auxnum = 0,
+	 	 this.opcion = 0,
+	 	 this.auxresultado = 0,
+	 	 this.viewdisplay();
+	  },
+	  /* funcion estado negativo o positivo*/
+	  sign: function(){
+	 	 this.animacion1("sign");
+	 	 if(this.pantalla != 0){
+	 		 if(this.signo == 0){
+	 			 this.pantalla = "-" + this.pantalla,
+	 			 this.signo = 1;
+	 		 }else{
+	 			 this.pantalla = this.pantalla.slice(1);
+	 			 this.signo = 0;
+	 		 }
+	 	 }
+	 	 this.viewdisplay();
+	  },
+	  /*funcion decimal*/
+	  punto: function(){
+	 	 this.animacion1("punto");
+	 	 if(this.decimal == 0){
+	 		 this.pantalla += ".";
+	 	 }
+	 	 this.decimal = 1,
+	 	 this.viewdisplay();
+	  },
+	  /*imprime en pantalla*/
+	  viewdisplay: function(){
+	 	 if(this.pantalla.toString().length > this.controlen){
+	 		 this.pantalla = this.pantalla.toString().substring(0,8);
+	 	 }
+	 	 document.getElementById("display").innerHTML = this.pantalla;
+	  }
+	 }
+	 Calculadora.inicio();
