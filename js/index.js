cea06a87fa1131ee49b2e885fa16e6f8cfa1c39e
Sebas <sebastian.buitrago@gmail.com>
2017-09-07 17:53:54 -0500

    Funciones
    suma, resta, por, división

--- js/index.js
+++ js/index.js
@@ -14,5 +14,63 @@ var Calculadora = {
 		function(){
 			this.EventosClick();
 		}
+	),
+	/*animar los botones al hacer click*/
+	animacionuno: function(tecla){
+		document.getElementById(tecla).style.transform="scale(0.9)";
+		setTimeout(function() {document.getElementById(tecla).style.transform="scale(1)";}, 200);
+	},
+	/*funcion restar*/
+		menos: function(){
+			this.animacionuno("menos");
+			this.num1 = Number(this.pantalla);
+			this.pantalla = "",
+			this.opcion = 2,
+			this.auxestado = 0,
+			this.signo = 0,
+			this.auxnum = 0,
+			this.auxestado = 0,
+			this.decimal = 0,
+			this.viewdisplay();
+	},
+	/*funcion multiplica*/
+		por: function(){
+			this.animacionuno("por");
+			this.num1 = Number(this.pantalla),
+			this.pantalla = "",
+			this.opcion = 3,
+			this.auxestado = 0,
+			this.signo = 0,
+			this.auxnum = 0,
+			this.auxestado = 0,
+			this.decimal = 0,
+			this.viewdisplay();
+	},
+	/*funcion dividir*/
+		dividido: function(){
+			this.animacionuno("dividido");
+			this.num1 = Number(this.pantalla),
+			this.pantalla = "",
+			this.opcion = 4,
+			this.auxestado = 0,
+			this.signo = 0,
+			this.auxnum = 0,
+			this.auxestado = 0,
+			this.decimal = 0,
+			this.viewdisplay();
+	},
+	/*funcion sumar*/
+	mas: function(){
+			this.animacionuno("mas");
+			this.num1 += Number(this.pantalla),
+			this.pantalla = "",
+			this.opcion = 1,
+			this.auxestado = 0,
+			this.signo = 0,
+			this.auxnum = 0,
+			this.auxestado = 0,
+			this.decimal = 0,
+			this.viewdisplay();
+ }
 }
 Calculadora.inicio();
